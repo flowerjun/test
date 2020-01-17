b275bdd84dea82a11ae47da38110838fa3849ddf
flowerjun <flowerjun@tmon.co.kr>
2020-01-17 11:35:14 +0900

    Updates

---
+++ precache-manifest.50267d66daddcb9a62ddaeb6c1369659.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2161ca88a6ee5f9a44914ec7aed3cce",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "3aeffb028788a4e51176",
+    "url": "/test/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "65a8cfc4269a89e15b64",
+    "url": "/test/static/js/2.f4e96d41.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/test/static/js/2.f4e96d41.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3aeffb028788a4e51176",
+    "url": "/test/static/js/main.3efbe7de.chunk.js"
+  },
+  {
+    "revision": "06ffb56752508a039c0e",
+    "url": "/test/static/js/runtime-main.68be2315.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test/static/media/logo.5d5d9eef.svg"
+  }
+]);
